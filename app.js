3ea439b01d05fabf0864e6698376f3853bb481ae
Matt Stuhring <whereyouatmatt@gmail.com>
2016-06-01 13:24:52 -0700

    geolocation works

--- app.js
+++ app.js
@@ -4,222 +4,146 @@
 // Declare variables and create empyt arrays
 var coffeeShopId = [];
 var coffeeShopContent = [];
-var allLatLng = []; //returned from the API
+var allLatLng = [];
 var allMarkers = [];
 var map;
-var infowindow;
+var userCords;
+var pos;
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map-canvas'), {
-    zoom: 8,
-    center: new google.maps.LatLng(47.6062, -122.3321),
-    panControl: false,
-    panControlOptions: {
-      position: google.maps.ControlPosition.BOTTOM_LEFT
-    },
+  var mapOptions = {
+    zoom: 4,
+    center: new google.maps.LatLng(37.09024, -95.712891),
     zoomControl: true,
     zoomControlOptions: {
       style: google.maps.ZoomControlStyle.LARGE,
       position: google.maps.ControlPosition.RIGHT_CENTER
-    },
-    scaleControl: false
-  });
+    }
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
-//grab form data from desktop nav bar
-$('.formDesktopNav').submit(function(event) {
 
-  coffeeShopContent = [];
-  var $city = $('#chooseCity2').val();
+
+//grab form input from desktop nav bar
+$('.formDesktopNav').submit(function(event) {
   event.preventDefault();
 
-  if ($city.length === 0) {
+  coffeeShopContent = [];
+  var $cityNav = $('#chooseCity2').val();
+
+  if ($cityNav.length === 0) {
     Materialize.toast('Please fill in search', 4000);
     return;
   }
-
-  var getCoffeeId = function(city) {
-
-    var $xhr = $.getJSON(`https://api.foursquare.com/v2/venues/search?client_id=UKXBEBLHQL3OZCTIRTNXWLGZZH3UBCV1PRHMYMJ2UOOUWVS5&client_secret=XRAB0PTCW1RDQMAU5W1S3YMSXTZUFUUV01CYHTWQHR2T0LCM&v=20130815&near=${city}&categoryId=4bf58dd8d48988d1e0931735&radius=3200`);
-
-    $xhr.done(function(categoryId) {
-      if ($xhr.status !== 200) {
-          return;
-      }
-      var venueIds = categoryId.response.venues;
-      // console.log(contents.response.venues);
-
-      for (var venueId of venueIds) {
-        var venue = {
-          id: venueId.id
-        };
-        coffeeShopId.push(venue);
-        getCoffeeContent(venue);
-      }
-    });
-  };
-  getCoffeeId($city);
-
-
-  // RESPONSE CHECKING FOR COFFEE SHOP WIFI ACCESS - yes or no
-  var getCoffeeContent = function(venue) {
-
-    var $xhr2 = $.getJSON(`https://api.foursquare.com/v2/venues/${venue.id}?client_id=UKXBEBLHQL3OZCTIRTNXWLGZZH3UBCV1PRHMYMJ2UOOUWVS5&client_secret=XRAB0PTCW1RDQMAU5W1S3YMSXTZUFUUV01CYHTWQHR2T0LCM&v=20130815`);
-
-    $xhr2.done(function(contents) {
-      if ($xhr2.status !== 200) {
-        return;
-      }
-      //grabs all data of each venue
-      var biz = contents.response.venue;
-      hasWifi(biz);
-
-      if (hasWifi(biz) === true) {
-        var latitude = biz.location.lat;
-        var longitude = biz.location.lng;
-        var venueName = biz.name
-        var address1 = biz.location.formattedAddress[0];
-        var address2 = biz.location.formattedAddress[1];
-        var address3 = biz.location.formattedAddress[2];
-        var hours = biz.hours.status;
-        var fourSquareVenueUrl = biz.shortUrl;
-
-        var contentString = '<div class="infowindow"><h5>' + venueName + '</h5><p>' + address1 + '</p><p>' + address2 + '</p><p>' + address3 + '</p><p>' + hours + '</p><a href="fourSquareVenueUrl">' + venueName + '</a></div>';
-
-        var myLatLng = new google.maps.LatLng(latitude, longitude);
-
-        var infowindow = new google.maps.InfoWindow({
-          content: contentString
-        });
-        var marker = new google.maps.Marker({
-          position: myLatLng,
-          map: map,
-        });
-        marker.addListener('click', function() {
-          console.log('clicked');
-          infowindow.setContent(contentString);
-          infowindow.open(map, marker);
-        });
-
-        //put all lat long in array
-				allLatLng.push(myLatLng);
-      }
-    });
-  };
-
-	//  Make an array of the LatLng's of the markers you want to show
-	//  Create a new viewpoint bound
-	var bounds = new google.maps.LatLngBounds ();
-	//  Go through each...
-	for (var i = 0, LtLgLen = allLatLng.length; i < LtLgLen; i++) {
-	  //  And increase the bounds to take this point
-	  bounds.extend (allLatLng[i]);
-	}
-	//  Fit these bounds to the map
-	map.fitBounds (bounds);
+  getCoffeeId($cityNav);
 });
 
 
-//grab form data from side nav bar
-// $('.formSideNav').submit(function(event) {
-//
-//   coffeeShopContent = [];
-//   var $city = $('#chooseCity').val();
-//   event.preventDefault();
-// console.log($city);
-//
-//   if ($city.length === 0) {
-//     Materialize.toast('Please fill in search', 4000);
-//     return;
-//   }
-//
-//   var getCoffeeId = function(city) {
-//
-//     var $xhr = $.getJSON(`https://api.foursquare.com/v2/venues/search?client_id=UKXBEBLHQL3OZCTIRTNXWLGZZH3UBCV1PRHMYMJ2UOOUWVS5&client_secret=XRAB0PTCW1RDQMAU5W1S3YMSXTZUFUUV01CYHTWQHR2T0LCM&v=20130815&near=${city}&categoryId=4bf58dd8d48988d1e0931735`);
-//
-//     $xhr.done(function(categoryId) {
-//       if ($xhr.status !== 200) {
-//           return;
-//       }
-//       var venueIds = categoryId.response.venues;
-//       // console.log(contents.response.venues);
-//
-//       for (var venueId of venueIds) {
-//         var venue = {
-//           id: venueId.id
-//         };
-//         coffeeShopId.push(venue);
-//         getCoffeeContent(venue);
-//       }
-//     });
-//   };
-//   getCoffeeId($city);
-//
-//
-//   // RESPONSE CHECKING FOR COFFEE SHOP WIFI ACCESS - yes or no
-//   var getCoffeeContent = function(venue) {
-//
-//     var $xhr2 = $.getJSON(`https://api.foursquare.com/v2/venues/${venue.id}?client_id=UKXBEBLHQL3OZCTIRTNXWLGZZH3UBCV1PRHMYMJ2UOOUWVS5&client_secret=XRAB0PTCW1RDQMAU5W1S3YMSXTZUFUUV01CYHTWQHR2T0LCM&v=20130815&near=seattle`);
-//
-//     $xhr2.done(function(contents) {
-//       if ($xhr2.status !== 200) {
-//         return;
-//       }
-//       //grabs all data of each venue
-//       var biz = contents.response.venue;
-//       hasWifi(biz);
-//
-//       if (hasWifi(biz) === true) {
-//         coffeeShopContent.push(biz.name);
-//         var latitude = biz.location.lat;
-//         var longitude = biz.location.lng;
-//         myLatLng = new google.maps.LatLng(latitude, longitude);
-//         allMarkers = new google.maps.Marker({
-//           position: myLatLng,
-//           map: map,
-//           html:
-// 						'<div>' +
-// 						'<h1>' + biz.name + '</h1>' +
-// 						'<h3>' + biz.location.formattedAddress[0] + '</h3>' +
-// 						'<p>' + biz.location.formattedAddress[1] + '</p>' +
-// 						'<p>' + biz.location.formattedAddress[2] + '</p>' +
-// 						'</div>'
-//         });
-//         //put all lat long in array
-// 				allLatLng.push(myLatLng);
-//       }
-//     });
-//   };
-//
-//   google.maps.event.addListener(allMarkers, 'click', function () {
-//     console.log(this);
-// 		infowindow.setContent(this.html);
-// 		infowindow.open(map, this);
-// 	});
-//
-//   //console.log(allLatlng);
-// 	//  Make an array of the LatLng's of the markers you want to show
-// 	//  Create a new viewpoint bound
-// 	var bounds = new google.maps.LatLngBounds ();
-// 	//  Go through each...
-// 	for (var i = 0, LtLgLen = allLatLng.length; i < LtLgLen; i++) {
-// 	  //  And increase the bounds to take this point
-// 	  bounds.extend (allLatLng[i]);
-// 	}
-// 	//  Fit these bounds to the map
-// 	map.fitBounds (bounds);
-//
-//
-//
-//
-//   // filter venues on whether or not they have wifi with a boolean of true/false
-//   var hasWifi = function(biz) {
-//     var properties = biz.attributes.groups;
-//     return properties.filter(function(prop) {
-//       return prop.name === 'Wi-Fi' && prop.items[0].displayValue === 'Yes';
-//     }).length > 0;
-//   };
-// });
+
+//grab form input from side nav bar
+$('.formSideNav').submit(function(event) {
+  event.preventDefault();
+
+  coffeeShopContent = [];
+  var $citySideNav = $('#chooseCity').val();
+
+  if ($citySideNav.length === 0) {
+    Materialize.toast('Please fill in search', 4000);
+    return;
+  }
+  getCoffeeId($citySideNav);
+});
+
+
+
+// Response to get all venues near a city with a coffee shop Id
+var getCoffeeId = function(city) {
+
+  var $xhr = $.getJSON(`https://api.foursquare.com/v2/venues/search?client_id=UKXBEBLHQL3OZCTIRTNXWLGZZH3UBCV1PRHMYMJ2UOOUWVS5&client_secret=XRAB0PTCW1RDQMAU5W1S3YMSXTZUFUUV01CYHTWQHR2T0LCM&v=20130815&near=${city}&categoryId=4bf58dd8d48988d1e0931735&radius=3200`);
+
+  $xhr.done(function(categoryId) {
+    if ($xhr.status !== 200) {
+        return;
+    }
+    var venueIds = categoryId.response.venues;
+
+    for (var venueId of venueIds) {
+      var venue = {
+        id: venueId.id
+      };
+      coffeeShopId.push(venue);
+      getCoffeeContent(venue);
+    }
+  });
+};
+
+
+// RESPONSE CHECKING FOR COFFEE SHOP WIFI ACCESS - yes or no
+var getCoffeeContent = function(venue) {
+
+  var $xhr2 = $.getJSON(`https://api.foursquare.com/v2/venues/${venue.id}?client_id=UKXBEBLHQL3OZCTIRTNXWLGZZH3UBCV1PRHMYMJ2UOOUWVS5&client_secret=XRAB0PTCW1RDQMAU5W1S3YMSXTZUFUUV01CYHTWQHR2T0LCM&v=20130815`);
+
+  $xhr2.done(function(contents) {
+    if ($xhr2.status !== 200) {
+      return;
+    }
+    //grabs all data of each venue
+    var biz = contents.response.venue;
+
+    if (hasWifi(biz)) {
+      var latitude = biz.location.lat;
+      var longitude = biz.location.lng;
+      var venueName = biz.name
+      var address1 = biz.location.formattedAddress[0];
+      var address2 = biz.location.formattedAddress[1];
+      var address3 = biz.location.formattedAddress[2];
+      var hours = biz.hours.status;
+      var fourSquareVenueUrl = biz.shortUrl;
+
+      var contentString = '<div class="infowindow"><h5>' + venueName + '</h5><p>' + address1 + '</p><p>' + address2 + '</p><p>' + address3 + '</p><p>' + hours + '</p><a href="fourSquareVenueUrl">' + venueName + '</a></div>';
+
+      var myLatLng = new google.maps.LatLng(latitude, longitude);
+
+      var infowindow = new google.maps.InfoWindow({
+        content: contentString,
+      });
+
+      var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map
+      });
+
+      marker.addListener('click', function() {
+        infowindow.setContent(contentString);
+        infowindow.open(map, marker);
+      });
+
+      // marker.addListener('mouseover', function() {
+      //   infowindow.open(map, marker);
+      // });
+      //
+      // marker.addListener('mouseout', function() {
+      //   infowindow.close(map, marker);
+      // });
+
+      //put all lat long in array
+			allLatLng.push(myLatLng);
+
+      //  Make an array of the LatLng's of the markers you want to show
+    	//  Create a new viewpoint bound
+    	var bounds = new google.maps.LatLngBounds();
+
+    	//  Go through each...
+    	for (var i = 0, LtLgLen = allLatLng.length; i < LtLgLen; i++) {
+    	  //  And increase the bounds to take this point
+    	  bounds.extend(allLatLng[i]);
+    	}
+    	//  Fit these bounds to the map
+    	map.fitBounds(bounds);
+    }
+  });
+};
 
 // filter venues on whether or not they have wifi with a boolean of true/false
 var hasWifi = function(biz) {
@@ -235,14 +159,77 @@ var hasWifi = function(biz) {
 
 
 
+//Start geolocation for desktop nav bar
+$('#myLocNav').on('click', function(event) {
 
+  if (navigator.geolocation) {
 
+  	function error(err) {
+  		console.warn('ERROR(' + err.code + '): ' + err.message);
+  	}
 
+  	function success(pos){
+  		userCords = pos.coords;
+      var userLat = userCords.latitude;
+      var userLng = userCords.longitude;
+      getCoffeeIdByUserLatLng(userLat, userLng);
+  		//return userCords;
+  	}
 
+	// Get the user's current position
+	navigator.geolocation.getCurrentPosition(success, error);
+	//console.log(pos.latitude + " " + pos.longitude);
+	} else {
+		alert('Geolocation is not supported in your browser');
+	}
+});
+//End Geo location
 
+//Start geolocation for desktop nav bar
+$('#myLocSideNav').on('click', function(event) {
 
+  if (navigator.geolocation) {
 
+  	function error(err) {
+  		console.warn('ERROR(' + err.code + '): ' + err.message);
+  	}
 
+  	function success(pos){
+  		userCords = pos.coords;
+      var userLat = userCords.latitude;
+      var userLng = userCords.longitude;
+      getCoffeeIdByUserLatLng(userLat, userLng);
+  		//return userCords;
+  	}
+
+	// Get the user's current position
+	navigator.geolocation.getCurrentPosition(success, error);
+	//console.log(pos.latitude + " " + pos.longitude);
+	} else {
+		alert('Geolocation is not supported in your browser');
+	}
+});
+//End Geo location
+
+// Get venues with wifi by user lat and lng
+var getCoffeeIdByUserLatLng = function(userLat, userLng) {
+  var $xhr = $.getJSON(`https://api.foursquare.com/v2/venues/search?client_id=UKXBEBLHQL3OZCTIRTNXWLGZZH3UBCV1PRHMYMJ2UOOUWVS5&client_secret=XRAB0PTCW1RDQMAU5W1S3YMSXTZUFUUV01CYHTWQHR2T0LCM&v=20130815&ll=${userLat},${userLng}&categoryId=4bf58dd8d48988d1e0931735&radius=3200`);
+
+  $xhr.done(function(categoryId) {
+    if ($xhr.status !== 200) {
+        return;
+    }
+    var venueIds = categoryId.response.venues;
+
+    for (var venueId of venueIds) {
+      var venue = {
+        id: venueId.id
+      };
+      coffeeShopId.push(venue);
+      getCoffeeContent(venue);
+    }
+  });
+};
 
 
 
