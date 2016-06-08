2c18f80e6b0fbc040a1160721ac8331f64a71938
Matt Stuhring <whereyouatmatt@gmail.com>
2016-06-08 10:04:01 -0700

    query search by coffee, starbucks

--- app.js
+++ app.js
@@ -70,7 +70,9 @@ $('.formSideNav').submit(function(event) {
 // Response to get all venues near a city with a coffee shop Id
 var getCoffeeId = function(city) {
 
-  var $xhr = $.getJSON(`https://api.foursquare.com/v2/venues/search?client_id=UKXBEBLHQL3OZCTIRTNXWLGZZH3UBCV1PRHMYMJ2UOOUWVS5&client_secret=XRAB0PTCW1RDQMAU5W1S3YMSXTZUFUUV01CYHTWQHR2T0LCM&v=20130815&near=${city}&categoryId=4bf58dd8d48988d1e0931735&categoryId=4bf58dd8d48988d16d941735&radius=3200`);
+///// FOR UNKNOWN REASONS FOURSQUARE API SEARCH BY CATEGORYID FOR COFFEE SHOPS HAS STOPPED WORKING AND NOW I AM SEARCHING BY QUERY=COFFEE,STARBUCKS
+
+  var $xhr = $.getJSON(`https://api.foursquare.com/v2/venues/search?client_id=UKXBEBLHQL3OZCTIRTNXWLGZZH3UBCV1PRHMYMJ2UOOUWVS5&client_secret=XRAB0PTCW1RDQMAU5W1S3YMSXTZUFUUV01CYHTWQHR2T0LCM&v=20130815&near=${city}&query=coffee,starbucks&radius=3200`);
 
   $xhr.done(function(categoryId) {
     if ($xhr.status !== 200) {
@@ -229,7 +231,10 @@ $('#myLocSideNav').on('click', function(event) {
 
 // response to foursquare venues with wifi by user lat and lng
 var getCoffeeIdByUserLatLng = function(userLat, userLng) {
-  var $xhr = $.getJSON(`https://api.foursquare.com/v2/venues/search?client_id=UKXBEBLHQL3OZCTIRTNXWLGZZH3UBCV1PRHMYMJ2UOOUWVS5&client_secret=XRAB0PTCW1RDQMAU5W1S3YMSXTZUFUUV01CYHTWQHR2T0LCM&v=20130815&ll=${userLat},${userLng}&categoryId=4bf58dd8d48988d1e0931735&radius=3200`);
+
+///// FOR UNKNOWN REASONS FOURSQUARE API SEARCH BY CATEGORYID FOR COFFEE SHOPS HAS STOPPED WORKING AND NOW I AM SEARCHING BY QUERY=COFFEE,STARBUCKS
+
+  var $xhr = $.getJSON(`https://api.foursquare.com/v2/venues/search?client_id=UKXBEBLHQL3OZCTIRTNXWLGZZH3UBCV1PRHMYMJ2UOOUWVS5&client_secret=XRAB0PTCW1RDQMAU5W1S3YMSXTZUFUUV01CYHTWQHR2T0LCM&v=20130815&ll=${userLat},${userLng}&query=coffee,starbucks&radius=3200`);
 
   $xhr.done(function(categoryId) {
     if ($xhr.status !== 200) {
