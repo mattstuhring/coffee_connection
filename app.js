2132e07d4d261f28bd0d9ca02a782465b17bd64a
Matt Stuhring <whereyouatmatt@gmail.com>
2016-10-24 12:01:27 -0700

    cleaned up code

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var coffeeShopContent = [];
 var allLatLng = [];
 var allMarkers = [];
 var map;
+let infowindow;
 var userCords;
 var pos;
 var styles = [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"on"},{"hue":"#0066ff"},{"saturation":74},{"lightness":100}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"on"},{"weight":0.6},{"saturation":-85},{"lightness":61}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.local","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#5f94ff"},{"lightness":26},{"gamma":5.86}]}];
@@ -34,10 +35,13 @@ function initMap() {
    google.maps.event.trigger(map, "resize");
    map.setCenter(center);
   });
+
+  // create infowindow to display venue content
+  infowindow = new google.maps.InfoWindow();
 }
 
-//grab form input from desktop nav bar
-$('.formDesktopNav').submit(function(event) {
+
+function userInput(event) {
   event.preventDefault();
 
   coffeeShopId = [];
@@ -45,36 +49,33 @@ $('.formDesktopNav').submit(function(event) {
   allLatLng = [];
   allMarkers = [];
 
-  var $cityNav = $('#chooseCity2').val();
+  var $city1 = $('#city1').val();
+  console.log('city 1', $city1);
 
-  if ($cityNav.length === 0) {
+  var $city2 = $('#city2').val();
+  console.log('city 2', $city2);
+
+  if ($city1.length === 0 && $city2.length === 0) {
     Materialize.toast('Please fill in search', 4000);
     return;
   }
-  getCoffeeId($cityNav);
-});
 
-//grab form input from side nav bar
-$('.formSideNav').submit(function(event) {
-  event.preventDefault();
-
-  coffeeShopId = [];
-  coffeeShopContent = [];
-  allLatLng = [];
-  allMarkers = [];
-
-  var $citySideNav = $('#chooseCity').val();
-
-  if ($citySideNav.length === 0) {
-    Materialize.toast('Please fill in search', 4000);
-    return;
+  if ($city1.length > 0 && $city2.length === 0) {
+    getCoffeeId($city1);
   }
-  getCoffeeId($citySideNav);
-});
+
+  if ($city1.length === 0 && $city2.length > 0) {
+    getCoffeeId($city2);
+  }
+}
+
+$('.formSideNav').submit(userInput);
+$('.formDesktopNav').submit(userInput);
+
+
 
 // Response to get all venues near a city with a coffee shop Id
 var getCoffeeId = function(city) {
-
   var $xhr = $.getJSON(`https://api.foursquare.com/v2/venues/search?client_id=UKXBEBLHQL3OZCTIRTNXWLGZZH3UBCV1PRHMYMJ2UOOUWVS5&client_secret=XRAB0PTCW1RDQMAU5W1S3YMSXTZUFUUV01CYHTWQHR2T0LCM&v=20130815&near=${city}&categoryId= 4bf58dd8d48988d1e0931735&limit=50&radius=3200`);
 
   $xhr.done(function(categoryId) {
@@ -100,7 +101,6 @@ var getCoffeeId = function(city) {
 
 // RESPONSE CHECKING FOR COFFEE SHOP WIFI ACCESS - yes or no
 var getCoffeeContent = function(venue) {
-
   var $xhr2 = $.getJSON(`https://api.foursquare.com/v2/venues/${venue.id}?client_id=UKXBEBLHQL3OZCTIRTNXWLGZZH3UBCV1PRHMYMJ2UOOUWVS5&client_secret=XRAB0PTCW1RDQMAU5W1S3YMSXTZUFUUV01CYHTWQHR2T0LCM&v=20130815`);
 
   $xhr2.done(function(contents) {
@@ -113,7 +113,7 @@ var getCoffeeContent = function(venue) {
     if (hasWifi(biz)) {
       var latitude = biz.location.lat;
       var longitude = biz.location.lng;
-      var venueName = biz.name
+      var venueName = biz.name;
       var address1 = biz.location.formattedAddress[0];
       var address2 = biz.location.formattedAddress[1];
       var address3 = biz.location.formattedAddress[2];
@@ -122,7 +122,7 @@ var getCoffeeContent = function(venue) {
 
       var contentString = `<div class="infowindow row">
         <div class="col s12">
-          <h4>${venueName}</h4>
+          <h4 class="center-align">${venueName}</h4>
           <p>${address1}</p>
           <p>${address2}</p>
           <p>${address3}</p>
@@ -132,11 +132,6 @@ var getCoffeeContent = function(venue) {
       </div>`;
 
       var myLatLng = new google.maps.LatLng(latitude, longitude);
-
-      // create infowindow to display venue content
-      var infowindow = new google.maps.InfoWindow({
-        content: contentString,
-      });
 
       // image for custom google map marker - blue coffee cup
       var image = {
@@ -157,6 +152,7 @@ var getCoffeeContent = function(venue) {
 
       allMarkers.push(marker);
 
+
       //store all lat long in array
 			allLatLng.push(myLatLng);
 
@@ -175,6 +171,8 @@ var getCoffeeContent = function(venue) {
   });
 };
 
+
+
 // filter venues on whether or not they have wifi with a boolean of true/false
 var hasWifi = function(biz) {
   var properties = biz.attributes.groups;
@@ -183,9 +181,9 @@ var hasWifi = function(biz) {
   }).length > 0;
 };
 
-//Start geolocation for desktop nav bar
-$('#myLocNav').on('click', function(event) {
 
+// GEOLOCATION from user
+function geolocation(event) {
   coffeeShopId = [];
   coffeeShopContent = [];
   allLatLng = [];
@@ -213,41 +211,11 @@ $('#myLocNav').on('click', function(event) {
 	} else {
     Materialize.toast('Geolocation is not supported in your browser', 4000);
 	}
-});
-//End Geo location for desktop nav bar
+}
 
-//Start geolocation for side nav bar
-$('#myLocSideNav').on('click', function(event) {
+$('#myLocSideNav').on('click', geolocation);
+$('#myLocNav').on('click', geolocation);
 
-  coffeeShopId = [];
-  coffeeShopContent = [];
-  allLatLng = [];
-  allMarkers = [];
-
-  Materialize.toast('Locate coffee shops with wifi?  Yes please!', 4000);
-
-  if (navigator.geolocation) {
-
-  	function error(err) {
-  		console.warn('ERROR(' + err.code + '): ' + err.message);
-  	}
-
-  	function success(pos){
-  		userCords = pos.coords;
-      var userLat = userCords.latitude;
-      var userLng = userCords.longitude;
-      getCoffeeIdByUserLatLng(userLat, userLng);
-  		//return userCords;
-  	}
-
-	// Get the user's current position
-	navigator.geolocation.getCurrentPosition(success, error);
-	//console.log(pos.latitude + " " + pos.longitude);
-	} else {
-    Materialize.toast('Geolocation is not supported in your browser', 4000);
-	}
-});
-//End Geo location for side nav
 
 // response to foursquare venues with wifi by user lat and lng
 var getCoffeeIdByUserLatLng = function(userLat, userLng) {
